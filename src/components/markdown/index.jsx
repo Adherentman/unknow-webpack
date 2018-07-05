ae1f3536dd73eeeeb76fdf1a0354f99609064611
XuZiHao <adherentxu@gmail.com>
2018-07-05 22:38:37 +0800

    add RichUtils

--- src/components/markdown/index.jsx
+++ src/components/markdown/index.jsx
@@ -1,17 +1,56 @@
 import React, { Component } from 'react';
-import { Editor, EditorState } from 'draft-js';
+import { Editor, EditorState, RichUtils } from 'draft-js';
 
 class MyEditor extends Component {
 	constructor(props) {
 		super(props);
 		this.state = { editorState: EditorState.createEmpty() };
 		this.onChange = editorState => this.setState({ editorState });
+		// this.handleKeyCommand = this.handleKeyCommand.bind(this);
 	}
+
+	handleKeyCommand = (command, editorState) => {
+		const newState = RichUtils.handleKeyCommand(editorState, command);
+		if (newState) {
+			this.onChange(newState);
+			console.log(newState.toJS());
+			return 'handled';
+		}
+		return 'not-handled';
+	};
+
+	onClickBold = () => {
+		this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
+	};
+
 	render() {
 		return (
-			<Editor editorState={this.state.editorState} onChange={this.onChange} />
+			<React.Fragment>
+				<div style={styles.editor}>
+					<Editor
+						editorState={this.state.editorState}
+						onChange={this.onChange}
+						placeholder="Enter some text..."
+						handleKeyCommand={this.handleKeyCommand}
+					/>
+				</div>
+				<button onClick={this.onClickBold}>粗体</button>
+			</React.Fragment>
 		);
 	}
 }
+const styles = {
+	root: {
+		fontFamily: "'Helvetica', sans-serif",
+		padding: 20,
+		width: 600
+	},
+	editor: {
+		border: '1px solid #ccc',
+		cursor: 'text',
+		minHeight: 80,
+		padding: 10
+	}
+};
 
 export default MyEditor;
